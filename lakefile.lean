dad36bb3d9aa432f53ad94059ecf4772b6b79b10
Sofia Rodrigues <sofia@algebraic.dev>
2024-08-30 10:43:39 -0300

    init: x.x

---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «lovelace» where
+  -- add package configuration options here
+
+lean_lib «Lovelace» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «lovelace» where
+  root := `Main
